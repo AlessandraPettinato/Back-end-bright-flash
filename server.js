20f750c9a4a64d9b67e0f089390429e6c08b8c01
Alessandra Pettinato <alessandrapettinato@Alessandras-MacBook-Pro.local>
2021-05-06 16:47:07 +0200

    Change port listen

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const data = require("./data");
 
 app.get("/", (req, res) => {
-  res.send(data);
+	res.send(data);
 });
 
-app.listen(5000, () => console.log("server running on port 5000"));
+app.listen(5000 || 3000, () => console.log("server running on port 5000"));
